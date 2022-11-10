a22b041cc3a54e561da80a07f42606fb0196dea9
IlyaDemidov <idemidov92@yandex.ru>
2022-11-10 20:45:58 +0300

    ДЗ № 2.2

---
+++ 2. Структуры и перечесления/Задача 2. Счета/Task_2.2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct Account
+{
+	int number{ 0 };
+	std::string name;
+	double balance{ 0 };
+};
+
+void input_account(Account& account)
+{
+	std::cout << "Введите номер счёта: ";
+	std::cin >> account.number;
+
+	std::cout << "Введите имя владельца: ";
+	std::cin >> account.name;
+
+	std::cout << "Введите баланс: ";
+	std::cin >> account.balance;
+}
+
+void print_account(Account& account)
+{
+	std::cout << "Ваш счёт: " << account.name << ", " << account.number << ", " << account.balance << "\n";
+}
+
+void new_balance(Account& account)
+{
+	std::cout << "Введите новый баланс: ";
+	std::cin >> account.balance;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+	Account account;
+	input_account(account);
+
+	print_account(account);
+
+	new_balance(account);
+
+	print_account(account);
+
+	system("pause");
+	return 0;
+}
