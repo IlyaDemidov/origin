9a5c8644b014f00d5c126ba8c6ea0a6976a554da
IlyaDemidov <idemidov92@yandex.ru>
2022-11-10 20:44:49 +0300

    ДЗ № 2.1

---
+++ 2. Структуры и перечесления/Задача 1. Месяцы/Task_2.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+enum Month
+{
+    January = 1, 
+    February, 
+    March, 
+    April,
+    May, 
+    June, 
+    July, 
+    August, 
+    September, 
+    October, 
+    November, 
+    December
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    while (true)
+    {
+        int number_month{ 0 };
+        std::cout << "Введите номер месяца: ";
+        std::cin >> number_month;
+
+        if (number_month == 0) { std::cout << "До свидания " << "\n"; break; }
+        if (number_month < 0 || number_month > 12) { std::cout << "Неправильный номер! " << "\n"; continue; }
+    
+        switch (static_cast<Month>(number_month))
+        {
+            case January: std::cout << "Январь" << "\n"; break;
+            case February: std::cout << "Фефраль" << "\n"; break;
+            case March: std::cout << "Март" << "\n"; break;
+            case April: std::cout << "Апрель" << "\n"; break;
+            case May: std::cout << "Май" << "\n"; break;
+            case June: std::cout << "Июнь" << "\n"; break;
+            case July: std::cout << "Июль" << "\n"; break;
+            case August: std::cout << "Август" << "\n"; break;
+            case September: std::cout << "Сентябрь" << "\n"; break;
+            case October: std::cout << "Октябрь" << "\n"; break;
+            case November: std::cout << "Ноябрь" << "\n"; break;
+            case December: std::cout << "Декабрь" << "\n"; break;
+
+            default: break;
+        }
+
+    }
+
+    system("pause");
+    return 0;
+}
