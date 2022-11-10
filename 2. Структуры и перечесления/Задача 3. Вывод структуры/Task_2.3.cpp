4996348d28fca3ace90cfa4f0d5aed0e4ee45898
IlyaDemidov <idemidov92@yandex.ru>
2022-11-10 20:47:07 +0300

    ДЗ № 2.3

---
+++ 2. Структуры и перечесления/Задача 3. Вывод структуры/Task_2.3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct Address
+{
+	std::string city;
+	std::string street;
+	int number_house{ 0 };
+	int number_flat{ 0 };
+	int index{ 0 };
+};
+
+void print_address(Address& address)
+{
+	std::cout << "Город: " << address.city << "\n"
+			  << "Улица: " << address.street << "\n"
+			  << "Номер дома: " << address.number_house << "\n"
+			  << "Номер квартиры: " << address.number_flat << "\n"
+			  << "Индекс: " << address.index << "\n" << "\n";
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	Address address_1{ "Москва", "Арбат", 12, 8, 123456 };
+	Address address_2{ "Ижевск", "Пушкина", 59, 143, 953769 };
+
+	print_address(address_1);
+	print_address(address_2);
+
+	system("pause");
+	return 0;
+}
