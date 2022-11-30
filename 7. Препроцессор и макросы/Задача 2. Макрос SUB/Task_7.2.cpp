54be453508260433a59bb7b0151df0ca60dbbef1
IlyaDemidov <idemidov92@yandex.ru>
2022-11-30 20:33:36 +0300

    ДЗ № 7.2

---
+++ 7. Препроцессор и макросы/Задача 2. Макрос SUB/Task_7.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+#define SUB(x, y)((x) - (y))
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    int a = 6;
+    int b = 5;
+    int c = 2;
+    std::cout << SUB(a, b) << std::endl;
+    std::cout << SUB(a, b) * c << std::endl;
+    std::cout << SUB(a, b + c) * c << std::endl;
+    
+        
+    std::cout << "\n";
+    system("pause");
+    return 0;
+   
+}
