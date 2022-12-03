057955ad90772bed7f0331e5d065ae8f232dec11
IlyaDemidov <idemidov92@yandex.ru>
2022-12-03 21:00:30 +0300

    ДЗ № 8.1

---
+++ 8. Исключения и обработка ошибок/Задача 1. Простое исключение/Task_8.1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <exception>
+
+int function(const std::string& str,const int& forbidden_length)
+{
+    size_t str_length{ str.length() };
+
+    if (str_length == forbidden_length) throw "bad_length";
+
+    return str_length;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите запретную длину: ";
+    int forbidden_length{ 0 };
+    std::cin >> forbidden_length;
+
+    std::string str;
+
+    while (true)
+    {
+        std::cout << "Введите слово: ";
+        std::cin >> str;
+
+        try
+        {
+            function(str, forbidden_length);
+        }
+
+        catch(const char* str)
+        {
+            std::cout << "Вы ввели слово запретной длины! До свидания";
+            break;
+        }
+        
+        std::cout << "Длина слова " << '"' << str << '"' << " равна " << function(str, forbidden_length) << "\n";
+    }
+
+
+    std::cout << "\n";
+    system("pause");
+    return 0;
+
+}
