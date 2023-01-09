3f8af5469833730c856a9b2d3718b40ffe109c48
IlyaDemidov <idemidov92@yandex.ru>
2023-01-09 23:59:40 +0300

    Update Camel

--- 0.Курсовой проект «Симулятор гонок»/Task_diplom/Racing_Simulator_Lib/Camel.cpp
+++ 0.Курсовой проект «Симулятор гонок»/Task_diplom/Racing_Simulator_Lib/Camel.cpp
@@ -7,7 +7,8 @@ Camel::Camel(double dist, const std::string& str)
 	double dist_no_pause = dist / 10;
 	int count_pause = (static_cast<int> (dist_no_pause) / 30);
 
-	if (count_pause >= 1) count_pause = ((count_pause - (count_pause - 1)) * 5) + ((count_pause - 1) * 8);
+	if (count_pause >= 1) count_pause = ((count_pause - (count_pause - 1)) * 5) + ((count_pause - 2) * 8);
+
 
 	this->race_time = dist_no_pause + count_pause;
 }
